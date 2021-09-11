ee4f1e397d3fb789fd649631f1d269264142a371
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-09-10 22:19:34 -0300

    Criação e configuração inicial das rotas

---
+++ src/pages/AuthLoading/AuthLoading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const AuthLoadingScreen: React.FC = () => {
+    return (
+        <Text>Tela de carregamento</Text>
+    )
+};
+
+export default AuthLoadingScreen;
---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Profile: React.FC = () => {
+    return (
+        <Text>User Profile</Text>
+    );
+};
+
+export default Profile;
