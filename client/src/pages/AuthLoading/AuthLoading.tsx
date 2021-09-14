07cf790859241b83d10a20f7ed8258dbdecfecc1
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-09-13 22:34:37 -0300

    Finalizado o roteamento entre loading e login/app

--- client/src/pages/AuthLoading/AuthLoading.tsx
+++ client/src/pages/AuthLoading/AuthLoading.tsx
@@ -1,11 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StatusBar, Text, ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { colors } from '../../styles';
 import { styles } from './styles';
+import { RootStackParamList } from '../RootStackParamList';
+import { NavigationStackScreenProps } from 'react-navigation-stack';
 
-const AuthLoadingScreen: React.FC = () => {
+type Props = NavigationStackScreenProps<RootStackParamList, 'Auth'>
 
-    const [ isLoading, setIsLoading ] = useState(true)
+const AuthLoadingScreen: React.FC<Props> = ({ navigation }) => {
+    const [ isLoading, setIsLoading ] = useState(true);
+
+    useEffect(() => {
+        bootstrapAsync();
+    }, []);
+
+    // Busca o token de autenticação do storage e redireciona para a página devida
+    const bootstrapAsync = async () => {
+        const userToken = await AsyncStorage.getItem('userToken');
+        
+        userToken ?? setIsLoading(false);
+
+        navigation.navigate(userToken ? 'App' : 'Auth');
+    };
 
     return (
         <>
