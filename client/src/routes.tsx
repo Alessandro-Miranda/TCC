02e7d0a5af0c7313e3526ba85b2e48d46ca57993
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-09-13 22:34:00 -0300

    Feito a definição da centralização dos header e ocultação do header na página de login

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -6,8 +6,27 @@ import Home from './pages/Home/Home';
 import Profile from './pages/Profile/Profile';
 import AuthLoadingScreen from './pages/AuthLoading/AuthLoading';
 
-const AppStack = createStackNavigator({ Home, Profile });
-const AuthStack = createStackNavigator({ SignIn })
+const AppStack = createStackNavigator(
+    { 
+        Home: Home,
+        Perfil: Profile
+    },
+    {
+        defaultNavigationOptions: {
+            headerTitleAlign: 'center'
+        }
+    }
+);
+const AuthStack = createStackNavigator(
+    {
+        Login: SignIn
+    },
+    {
+        defaultNavigationOptions: {
+            headerShown: false
+        }
+    }
+)
 
 const Routes = createAppContainer(
     createSwitchNavigator(
