a6dd8b9e8e48d506506ef22467710aaef63a8ba6
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-09-15 16:49:56 -0300

    Atulizado a frase na callback de inicio do server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import { authRouter } from './routes';
+
 const server = express();
 const PORT = process.env.PORT || 4000;
 
 server.use(express.urlencoded({ extended: true, limit: '50mb' }))
 server.use(express.json({ limit: '50mb' }));
 server.use('/auth', authRouter);
+
 server.listen(PORT, () => {
-    console.log('O servidor tá rodando nessa porra aqui hein');
+    console.log('Server is listening at the port: ', PORT);
 });
 
 export default server;
