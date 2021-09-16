2784dfd5ca43b5d16aa79462991070d915dbfb1f
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-09-16 14:57:00 -0300

    Finalizado a lógica de autenticação

--- server/src/routes/Auth.ts
+++ server/src/routes/Auth.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import { getDatabase, ref, onValue, query, orderByChild } from 'firebase/database';
+import { getDatabase, ref, onValue } from 'firebase/database';
 import { firebaseApp } from '../config/firebaseConfig';
+import { createHash } from 'crypto';
+import { sign } from 'jsonwebtoken';
 
-export const authRouter = Router();
-
-authRouter.get('/', (req, res) => {
-    res.send('vai tomar no cu')
-});
+const authRouter = Router();
 
 type ReqBodyAuth = {
     userId: string;
@@ -14,44 +12,62 @@ type ReqBodyAuth = {
 };
 
 type UserInformations = {
+    id: string;
     department: string;
     email: string;
     first_name: string;
     last_name: string;
     password: string;
+    admin: boolean;
 };
 
 authRouter.post('/', (req, res) => {
     const userInformations: ReqBodyAuth = req.body;
     const { userId, password } = userInformations;
-    const dbRef = ref(getDatabase(firebaseApp), '/users');
+    const HASHED_PASSWORD = createHash('sha256').update(password).digest('hex');
+    const DB_REF = ref(getDatabase(firebaseApp), '/users');
     
+    // Inicia o objeto com as propriedades vazias
     const user: UserInformations = {
+        id: '',
         department: '',
         email: '',
         first_name: '',
         last_name: '',
-        password: ''
+        password: '',
+        admin: false
     };
 
-    onValue(dbRef, (value) => {
+    onValue(DB_REF, (value) => {
         value.forEach(elem => elem.key === userId && Object.assign(user, elem.val()));
+        
+        if(Object.values(user).some(value => value === ''))
+        {
+            res.status(500).json({
+                message: 'Usuário não encontrado'
+            }).end();
+            return;
+        }
+        
+        if(HASHED_PASSWORD !== user.password)
+        {
+            res.status(500).json({
+                message: 'Senha incorreta'
+            }).end();
+            return;
+        }
+
+        const JWT_SECRET = process.env.JWT_SECRET?.trimEnd() ?? '';
+        const TOKEN = sign({ id: user.id }, JWT_SECRET, {
+            expiresIn: 600
+        });
+        
+        res.status(200).json({
+            auth: true,
+            TOKEN,
+            isAdmin: user.admin
+        }).end();
     }, { onlyOnce: true });
-
-    if(Object.values(user).every(value => value === ''))
-    {
-        res.send('Usuário não encontrado');
-        res.end();
-        return;
-    }
-
-    if(password !== user.password)
-    {
-        res.send('Senha incorreta');
-        res.end();
-        return;
-    }
-
-    // Configurar aqui a autenticação JWT
-    res.send('ok');
 });
+
+export { authRouter };
