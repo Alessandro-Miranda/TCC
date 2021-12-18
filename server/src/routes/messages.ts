2f7bae5f63e1b0d4b1916066c687df683859d92c
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-18 18:10:42 -0300

    Retirado o middleware

--- server/src/routes/messages.ts
+++ server/src/routes/messages.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
-import { verifyJWT } from '../utils/verifyJWT';
 
 const messagesRouter = Router();
 
-messagesRouter.get("/", verifyJWT, (req, res) => {
+messagesRouter.get("/", (req, res) => {
     console.log('aqui vai retornar os users');
     res.status(200).json({
         message: "Aqui vai retornar as mensagens trocadas pelo usuário"
