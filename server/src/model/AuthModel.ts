0cd9de17170ffe38b32f3dc6d697640ae8c13954
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-18 14:23:48 -0300

    Criado o model responsável pela validação de usuário

---
+++ server/src/model/AuthModel.ts
@@ -0,0 +1,58 @@
+import { collection } from 'firebase/firestore';
+import { sign } from 'jsonwebtoken';
+import { firestoreApp } from '../config/firebaseConfig';
+import { INVALID_PASSWORD, USERS_COLLECTION_NAME, USER_NOT_FOUND } from '../constants/constants';
+import { Database } from '../repositories/Database';
+import { User } from '../types/User';
+import { passwordValidate } from '../utils/passwordValidate';
+
+export class AuthModel
+{
+    private usersRef;
+    
+    constructor(
+        private database = new Database()
+    )
+    {
+        this.usersRef = this.getUsersRef();
+    }
+
+    getUsersRef()
+    {
+        return collection(firestoreApp, USERS_COLLECTION_NAME);
+    }
+
+    async checkUser(email: string, password: string)
+    {
+        const [ userFound ] = await this.database.findUserByEmail(this.usersRef, email);
+
+        if(!userFound)
+        {
+            return USER_NOT_FOUND;
+        }
+
+        const isValidPassword = passwordValidate(password, 'sha256', userFound.password);
+
+        if(!isValidPassword)
+        {
+            return INVALID_PASSWORD;
+        }
+        
+        const token = this.generateAuthToken(userFound);
+
+        return {
+            token,
+            user: userFound
+        };
+    }
+
+    generateAuthToken(user: User)
+    {
+        const jwtSecret = process.env.JWT_SECRET?.trimEnd() as string;
+        const token = sign({ id: user.email }, jwtSecret, {
+            expiresIn: 3600
+        });
+
+        return token;
+    }
+}
