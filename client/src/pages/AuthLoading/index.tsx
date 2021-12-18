734cfef0c91d2063d351017caa48a54968d02a7d
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-18 20:21:37 -0300

    Refatorado para utilziar a função auxiliar de validação de login

--- client/src/pages/AuthLoading/index.tsx
+++ client/src/pages/AuthLoading/index.tsx
@@ -2,11 +2,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, StatusBar, Text, View } from 'react-native';
 import { NavigationStackScreenProps } from 'react-navigation-stack';
-import { api } from '../../services/api';
 import { colors } from '../../styles';
+import { validateLogin } from '../../utils/validateLogin';
 import { RootStackParamList } from '../RootStackParamList';
 import { styles } from './styles';
-
 
 type Props = NavigationStackScreenProps<RootStackParamList, 'Auth'>
 
@@ -19,18 +18,8 @@ const AuthLoadingScreen: React.FC<Props> = ({ navigation }) => {
 
     const bootstrapAsync = async () => {
         const userToken = await AsyncStorage.getItem('authToken');
-        
-        if(!userToken) navigation.navigate('Auth');
-
-        const { data } = await api.get<{ auth: boolean }>('/auth/confirm-authentication', {
-            headers: {
-                "x-access-token": userToken
-            }
-        })
-        
-        data ?? setIsLoading(false);
-
-        navigation.navigate(data.auth ? 'App' : 'Auth');
+        setIsLoading(false);
+        await validateLogin(userToken, navigation, 'App');
     };
 
     return (
