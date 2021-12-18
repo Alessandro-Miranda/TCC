e2b8505294046cf43880c2db5cd430562cbf306a
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-18 18:10:06 -0300

    Incluido a rota de confirmação de autenticação

--- server/src/routes/Auth.ts
+++ server/src/routes/Auth.ts
@@ -3,7 +3,8 @@ import { AuthController } from '../controller/AuthController';
 
 const authRouter = Router();
 
-authRouter.post('/', (new AuthController()).post);
+authRouter.post('/', (new AuthController()).authenticate);
+authRouter.get('/confirm-authentication', (new AuthController()).confirmAuthentication)
 
 export { authRouter };
 
