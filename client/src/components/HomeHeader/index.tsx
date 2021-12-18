42f599afd334808674c8fe4d10c0154356b35348
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-18 20:22:04 -0300

    Adicionado botão temporário de saída

--- client/src/components/HomeHeader/index.tsx
+++ client/src/components/HomeHeader/index.tsx
@@ -1,16 +1,30 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
 import {
-    View,
+    Image,
     Text,
-    Image
+    View
 } from 'react-native';
+import { NavigationParams, NavigationRoute } from "react-navigation";
+import { StackNavigationProp } from "react-navigation-stack/lib/typescript/src/vendor/types";
 import { styles } from "./styles";
 
-const HomeHeader: React.FC = () => {
+type Props = {
+    navigation: StackNavigationProp<NavigationRoute<NavigationParams>, NavigationParams>
+}
+
+const HomeHeader: React.FC<Props> = ({ navigation }) => {
+    
+    const onExit = async () => {
+        await AsyncStorage.clear();
+        navigation.navigate('Auth');
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.HeaderTitle}>Mensages</Text>
             <Image source={require('../../../assets/searchIcon.png')} />
+            <Text onPress={onExit}>Sair</Text>
         </View>
     )
 }
