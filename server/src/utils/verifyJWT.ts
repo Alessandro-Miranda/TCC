ca1fbaded1be7f7edb81e7cac95a04b33ffe0ab6
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-18 18:11:26 -0300

    Refatorado a função de validação de token de autenticação

--- server/src/utils/verifyJWT.ts
+++ server/src/utils/verifyJWT.ts
@@ -1,28 +1,16 @@
-import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
-export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
-    const TOKEN = req.headers['x-access-token'] as string;
-    const SECRET_KEY = process.env.JWT_SECRET?.trimEnd() ?? '';
-    let decodedId: string;
+export function verifyJWT(token: string)
+{
+    const secretKey = process.env.JWT_SECRET?.trimEnd() as string;
+    let isUserAuthenticated = false;
 
-    if(!TOKEN)
-    {
-        return res.status(500).json({
-            auth: false,
-            message: 'No token provided'
-        }).end();
-    }
-
-    verify(TOKEN, SECRET_KEY, (err) => {
-        if(err)
+    verify(token, secretKey, (err) => {
+        if(!err)
         {
-            return res.status(500).json({
-                auth: false,
-                message: 'Failed to authenticate token'
-            });
+            isUserAuthenticated = true;
         }
-
-        next();
     });
+
+    return isUserAuthenticated;
 }
