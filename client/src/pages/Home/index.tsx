96e8d525b4887fec7580d95377ba4bf8256098fe
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-19 10:50:46 -0300

    Refatorado para divisão em componentes

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,18 +1,23 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-    FlatList, Text,
+    Image,
+    Text,
     View
 } from 'react-native';
-import { TouchableHighlight } from 'react-native-gesture-handler';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { NavigationStackScreenProps } from 'react-navigation-stack';
-import ProfileImage from '../../components/ProfileImage';
+import MessagePreview from '../../components/MessagePreview';
+import { Preview } from '../../types/Messages';
+import { RootStackParamList } from '../../types/RootStackParamList';
 import { validateLogin } from '../../utils/validateLogin';
-import { RootStackParamList } from '../RootStackParamList';
 import { styles } from './styles';
 
 type Props = NavigationStackScreenProps<RootStackParamList, 'Auth'>
+
 const Home: React.FC<Props> = ({ navigation }) => {
+
+    const [ messages, setMessages ] = useState<Preview>();
 
     useEffect(() => {
         bootstrapAsync();
@@ -23,50 +28,29 @@ const Home: React.FC<Props> = ({ navigation }) => {
         await validateLogin(token, navigation);
     }
 
-    const onMessageSelect = () => {
-
-    }
-
     return (
         <View style={styles.container}>
-            <FlatList
-                data={[
-                    { contactName: 'João', message: 'Alguma mensagem que você verá a introdução dela' },
-                    { contactName: 'Maria', message: 'Alguma mensagem que você verá a introdução dela' },
-                    { contactName: 'Elaine', message: 'Alguma mensagem que você verá a introdução dela' },
-                    { contactName: 'Kelly', message: 'Alguma mensagem que você verá a introdução dela' },
-                    { contactName: 'Rodrigo', message: 'Alguma mensagem que você verá a introdução dela' },
-                ]}
-                renderItem={({ item, index }) => (
-                    <TouchableHighlight
-                        style={styles.messageContainer}
-                        onPress={onMessageSelect}
-                        key={index}
-                    >
-                        <View style={styles.messageContainerView}>
-                            <ProfileImage
-                                uri={'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__480.png'}
-                                width={50}
-                                height={50}
-                                style={styles.profileImage}
-                            />
-                            <View style={styles.contactNameAndMessage}>
-                                <Text style={styles.contactName}>{item.contactName}</Text>
-                                <Text style={styles.messagePreview}>
-                                    {`${item.message.substring(0, 40)}...`}
-                                </Text>
-                            </View>
-                        </View>
-                    </TouchableHighlight>
-                )}
-                keyExtractor={(_, index) => index.toString()}
-                style={styles.flatList}
-            />
-            <View>
-                <Text style={styles.showContactsIcon}>Icon</Text>
+            {
+                messages ? <MessagePreview /> : <NoMessages />
+            }
+            <View style={styles.showContactsIcon}> 
+                <TouchableOpacity>
+                    <Image
+                        source={require('../../../assets/chat.png')}
+                        style={styles.showContactIconImage}
+                    />
+                </TouchableOpacity>
             </View>    
         </View>
     );
 }
+
+const NoMessages = () => (
+    <View style={[ styles.wrapper, styles.noMessage ]}>
+        <Text style={styles.noMessageText}>
+            Você ainda não possui nenhuma mensagem.
+        </Text>
+    </View>
+)
 
 export default Home;
