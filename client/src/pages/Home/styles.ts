46a1be2b34867cebba63f4390e8ffeece358cc26
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-19 10:48:32 -0300

    Criado o estilo da home sem mensagens

--- client/src/pages/Home/styles.ts
+++ client/src/pages/Home/styles.ts
@@ -3,42 +3,31 @@ import { colors } from '../../styles';
 
 export const styles = StyleSheet.create({
     container: {
-        position: 'relative'
+        flex: 1
     },
-    flatList: {
+    wrapper: {
         minHeight: '100%'
-    },
-    messageContainer: {
-        paddingBottom: 20,
-        paddingTop: 20,
-        paddingLeft: 20,
-        overflow: 'hidden',
-        borderBottomWidth: 1,
-        borderBottomColor: colors.secundary
-    },
-    messageContainerView: {
-        flex: 1,
-        flexDirection: 'row'
-    },
-    profileImage: {
-        borderRadius: 50
-    },
-    contactNameAndMessage: {
-        paddingLeft: 20
-    },
-    contactName: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        color: colors.secundary,
-        letterSpacing: 2
-    },
-    messagePreview: {
-        fontSize: 16,
-        color: colors.fontColor
     },
     showContactsIcon: {
         position: 'absolute',
         bottom: 50,
-        right: 50
+        right: 50,
+    },
+    showContactIconImage: {
+        width: 70,
+        height: 70,
+    },
+    noMessage: {
+        flex: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
+    },
+    noMessageText: {
+        textAlign: 'center',
+        fontSize: 20,
+        width: '80%',
+        alignSelf: 'center',
+        color: colors.secundary,
+        letterSpacing: 2
     }
 })
