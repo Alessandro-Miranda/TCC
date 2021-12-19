60f10525baa150c98946550ae2cbd219f6d66c43
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-19 18:02:30 -0300

    Criado os tipos de retorno de contatos e usuário

---
+++ client/src/types/User.ts
@@ -0,0 +1,16 @@
+export type Contact = {
+    chatID: string;
+    department: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+}
+
+export type User = {
+    first_name: string;
+    department: string;
+    email: string;
+    last_name: string;
+    password: string;
+    admin: boolean;
+}
