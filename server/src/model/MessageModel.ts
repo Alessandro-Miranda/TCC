f9cd2bbcae170bf5ce2779f02ae3cc7a61cf608a
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-26 16:25:04 -0300

    Criado o método para encontrar os usuários pelo departamento

--- server/src/model/MessageModel.ts
+++ server/src/model/MessageModel.ts
@@ -1,6 +1,9 @@
 import { USERS_COLLECTION_NAME } from "../constants/constants";
 import { Database } from "../repositories/Database";
 import { Contacts } from "../types/Contacts";
+import { User } from "../types/User";
+
+type AllUsers = Omit<User, "password">;
 
 export class MessageModel
 {
@@ -15,5 +18,22 @@ export class MessageModel
     {
         const contacts = await this.database.findAllContacts(username);
         return contacts;
+    }
+
+    async getAllUsersFromSameDepartment(email: string, department: string): Promise<AllUsers[]>
+    {
+        const users = await this.database.findAllUsersByDepartment(department);
+        const allContacts: AllUsers[] = [];
+        
+        for(const user of users)
+        {
+            const { password, ...userInfos } = user;
+
+            if(userInfos.email !== email)
+            {
+                allContacts.push(userInfos);
+            }
+        }
+        return allContacts;
     }
 }
