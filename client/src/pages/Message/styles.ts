b59d67cd6e1cf6a40c0e8779a04c5dc16ee13958
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-12-31 16:20:12 -0300

    Criado o estilo do container e input de mensage

---
+++ client/src/pages/Message/styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../styles";
+
+export const styles = StyleSheet.create({
+    messageContainer: {
+        paddingHorizontal: 10,
+        height: '100%',
+    },
+    messageWrapper: {
+        maxHeight: '89%'
+    },
+    messageInputContainer: {
+        flexDirection: 'row',
+        overflow: 'hidden',
+        position: 'absolute',
+        bottom: 0,
+        paddingHorizontal: 20,
+        marginBottom: 20,
+        width: '100%'
+    },
+    messageInput: {
+        borderStyle: 'solid',
+        borderColor: colors.secundary,
+        borderWidth: 1,
+        borderRadius: 50,
+        width: '90%',
+        height: 50,
+        paddingHorizontal: 20,
+        marginRight: 10
+    }
+});
