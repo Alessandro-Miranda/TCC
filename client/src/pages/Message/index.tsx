9cf3ed89eb07556d1e82b53607c3c0f85a3351a7
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-02 15:27:36 -0300

    Iniciado a criação do envio das mensagens

--- client/src/pages/Message/index.tsx
+++ client/src/pages/Message/index.tsx
@@ -1,40 +1,70 @@
 import React, { useEffect, useState } from "react";
 import { Image, NativeSyntheticEvent, TextInput, TextInputChangeEventData, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, TouchableHighlight } from "react-native-gesture-handler";
 import { NavigationStackScreenProps } from "react-navigation-stack";
 import MessagesText from "../../components/MessagesText";
-import { MessageBody } from "../../types/Messages";
+import { MessageBody, MessageState } from "../../types/Messages";
 import { RootStackParamList } from "../../types/RootStackParamList";
 import { Contact } from "../../types/User";
+import { getInformationsFromStorage } from "../../utils/getInformationsFromStorage";
 import { styles } from "./styles";
 
 type Props = NavigationStackScreenProps<RootStackParamList, "Message">;
 
 const Message: React.FC <Props> = ({ navigation }) => {
-    
-    const [ contact, setContact ] = useState({} as Contact);
-    const [ chatID, setChatID] = useState('');
     const [ message, setMessage ] = useState('');
     const [ allMessages, setAllMessages ] = useState([] as MessageBody[]);
-
+    const [ chatInfos, setChatInfos ] = useState({ chatID: '', contactInfo: {} as Contact });
+    
     useEffect(() => {
-        const contact = navigation.state.params?.contact as Contact;
+        const contactInfo = navigation.state.params?.contact as Contact;
         const chatID = navigation.state.params?.chatID;
-        
-        setContact(contact);
-        setAllMessages([
-            {
-                message: 'coe neguim',
-                from: 'joao@empresa.com',
-                timestamp: '1640975957317'
-            },
-        ]);
 
-        if(chatID !== "") setChatID(chatID);
+        setChatInfos({
+            chatID,
+            contactInfo
+        });
     }, []);
 
     const onTypeMessage = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
         setMessage(event.nativeEvent.text);
+    }
+
+    const onSendMessage = async () => {
+        const newMessage = [...allMessages];
+        const userInfo = await getInformationsFromStorage('user');
+
+        if(userInfo)
+        {
+            const { email } = JSON.parse(userInfo);
+
+            newMessage.push({
+                from: email,
+                message,
+                timestamp: new Date().getTime(),
+                state: MessageState.Wait
+            });
+            
+            setAllMessages(newMessage);
+
+            initChat();
+        }
+
+    }
+
+    const initChat = () => {
+        if(chatInfos.chatID !== "")
+        {
+
+        }
+        else
+        {
+            createChat();
+        }
+    }
+
+    const createChat = () => {
+
     }
 
     return (
@@ -63,11 +93,16 @@ const Message: React.FC <Props> = ({ navigation }) => {
                     multiline={true}
                     style={styles.messageInput}
                 />
-                <Image
-                    width={20}
-                    height={20}
-                    source={require('../../../assets/send.png')}
-                />
+                <TouchableHighlight
+                    underlayColor={'unset'}
+                    onPress={onSendMessage}
+                >
+                    <Image
+                        width={20}
+                        height={20}
+                        source={require('../../../assets/send.png')}
+                    />
+                </TouchableHighlight>
             </View>
         </View>
     )
