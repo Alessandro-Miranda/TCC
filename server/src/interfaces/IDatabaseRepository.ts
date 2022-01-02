cf668c9e3dc0f50bb3464560e536930a84d0a09f
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-02 18:33:18 -0300

    Adicionado os métodos para adicionar contato e criar chat

--- server/src/interfaces/IDatabaseRepository.ts
+++ server/src/interfaces/IDatabaseRepository.ts
@@ -5,7 +5,9 @@ import { User } from "../types/User";
 export interface IDatabaseRepositorie
 {
     getRef(collectionName: string): CollectionReference<DocumentData>;
-    findUserByEmail(email: string): Promise<User[]>;
+    findUserByEmail(email: string): Promise<User>;
     findAllUsersByDepartment(department: string): Promise<User[]>;
     findAllContacts(username: string): Promise<Contacts[]>;
+    createChat(userEmail: string, contactEmail: string, uniqueChatId: string): Promise<Boolean>;
+    addContact(userEmail: string, contactEmail: string, contactId: string, chatId: string): Promise<Boolean>;
 }
