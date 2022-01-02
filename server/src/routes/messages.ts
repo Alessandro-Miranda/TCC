cfae2f1c91cff3081928e804137c538ffb48519d
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-02 18:30:19 -0300

    Adicionado a rota para criação do chat

--- server/src/routes/messages.ts
+++ server/src/routes/messages.ts
@@ -5,5 +5,7 @@ const messagesRouter = Router();
 
 messagesRouter.get("/contacts/:email/:department", (req, res) => new MessageController().getContacts(req, res));
 
+messagesRouter.post("/create-chat", (req, res) => new MessageController().createChat(req, res));
+
 export { messagesRouter };
 
