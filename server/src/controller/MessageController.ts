6cd33ba62dc71d50ec936d234bd915e2b0680c1a
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-02 18:32:39 -0300

    Adicionado o método para criação dos chats

--- server/src/controller/MessageController.ts
+++ server/src/controller/MessageController.ts
@@ -7,6 +7,17 @@ type Params = {
     department?: string;
 }
 
+type CreateChatBody = {
+    userEmail: string;
+    contactEmail: string;
+}
+
+type ResponseErrorBody = {
+    status: number;
+    header: string[];
+    send: { message: string };
+}
+
 export class MessageController
 {
     private model;
@@ -20,27 +31,12 @@ export class MessageController
     {
         const { email, department } = req.params as Params;
         const token = req.headers['x-access-token'];
+    
+        const checkAuthentication = this.checkIfUserIsAuthenticated(token);
 
-        if(!token)
+        if(!checkAuthentication.continue)
         {
-            res
-                .status(401)
-                .setHeader('WWW-Authenticate', 'Unauthorized')
-                .send({ message: 'Usuário não logado' });
-                
-            return;
-        }
-
-        const authModel = new AuthModel();
-        const isAuthenticated = authModel.verifyJWT(token as string);
-
-        if(!isAuthenticated)
-        {
-            res
-                .status(401)
-                .setHeader('WWW-Authenticate', 'Unauthorized')
-                .send({ message: 'Token expirado' });
-                
+            this.sendRequestErrorResponse(res, checkAuthentication as ResponseErrorBody)
             return;
         }
         
@@ -56,5 +52,88 @@ export class MessageController
         }
 
         res.status(200).send(contacts);
+    }
+
+    async createChat(req: Request, res: Response)
+    {
+        const { userEmail, contactEmail } = req.body as CreateChatBody;
+        const token = req.headers['x-access-token'];
+
+        const checkAuthentication = this.checkIfUserIsAuthenticated(token);
+
+        if(!checkAuthentication.continue)
+        {
+            this.sendRequestErrorResponse(res, checkAuthentication as ResponseErrorBody)
+            return;
+        }
+
+        if(!userEmail || !contactEmail)
+        {
+            this.sendRequestErrorResponse(res, {
+                status: 422,
+                header: ['422', 'Unprocessabel Entity'],
+                send: { message: 'É necessário que seja passado, no mínimo, dois usuários para a criação do chat' }
+            });
+
+            return;
+        }
+
+        try
+        {
+            const chatID = await this.model.createChat(userEmail, contactEmail);
+            
+            res.status(200).send({ chatID });
+        }
+        catch(err)
+        {
+            this.sendRequestErrorResponse(res, {
+                status: 500,
+                header: ['500', 'Internal Server Error'],
+                send: { message: String(err) }
+            });
+
+            return;
+        }
+
+    }
+
+    private checkIfUserIsAuthenticated(token: string | string[] | undefined)
+    {
+        const authModel = new AuthModel();
+
+        if(!token)
+        {
+            return {
+                continue: false,
+                status: 401,
+                header: ['WWW-Authenticate', 'Unauthorized'],
+                send: { message: 'Usuário não logado' }
+            }
+        }
+
+        const isAuthenticated = authModel.verifyJWT(token as string);
+
+        if(!isAuthenticated)
+        {
+            return {
+                continue: false,
+                status: 401,
+                header: ['WWW-Authenticate', 'Unauthorized'],
+                send: { message: 'Token expirado' }
+            };
+        }
+
+        return { continue: true };
+    }
+
+    private sendRequestErrorResponse(res: Response, responseBody: ResponseErrorBody)
+    {
+        res
+            .status(responseBody.status)
+            .setHeader(
+                responseBody.header?.[0],
+                responseBody.header?.[1]
+            )
+            .send(responseBody.send);
     }
 }
