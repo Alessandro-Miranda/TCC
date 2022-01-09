d734efca35f1e995c7f728a2584b60bb5d199444
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-08 21:03:26 -0300

    Implementado o método de solicitação de criação de chat

--- client/src/pages/Message/index.tsx
+++ client/src/pages/Message/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Image, NativeSyntheticEvent, TextInput, TextInputChangeEventData, View } from "react-native";
+import { Alert, Image, NativeSyntheticEvent, TextInput, TextInputChangeEventData, View } from "react-native";
 import { ScrollView, TouchableHighlight } from "react-native-gesture-handler";
 import { NavigationStackScreenProps } from "react-navigation-stack";
+import { io, Socket } from "socket.io-client";
 import MessagesText from "../../components/MessagesText";
+import { api } from "../../services/api";
 import { MessageBody, MessageState } from "../../types/Messages";
 import { RootStackParamList } from "../../types/RootStackParamList";
-import { Contact } from "../../types/User";
+import { Contact, User } from "../../types/User";
 import { getInformationsFromStorage } from "../../utils/getInformationsFromStorage";
 import { styles } from "./styles";
 
@@ -13,10 +15,17 @@ type Props = NavigationStackScreenProps<RootStackParamList, "Message">;
 
 const Message: React.FC <Props> = ({ navigation }) => {
     const [ message, setMessage ] = useState('');
+    const [ userInfos, setUserInfos ] = useState({} as User);
     const [ allMessages, setAllMessages ] = useState([] as MessageBody[]);
     const [ chatInfos, setChatInfos ] = useState({ chatID: '', contactInfo: {} as Contact });
+    const [ socket, setSocket ] = useState<Socket>();
     
     useEffect(() => {
+        asyncBootstrap();
+        const socket = io("http://192.168.1.4:4000/");
+
+        setSocket(socket);
+        
         const contactInfo = navigation.state.params?.contact as Contact;
         const chatID = navigation.state.params?.chatID;
 
@@ -24,7 +33,20 @@ const Message: React.FC <Props> = ({ navigation }) => {
             chatID,
             contactInfo
         });
+
+        return () => {
+            socket.disconnect();
+        }
     }, []);
+
+    const asyncBootstrap = async () => {
+        const user = await getInformationsFromStorage('user');
+
+        if(user)
+        {
+            setUserInfos(JSON.parse(user));
+        }
+    }
 
     const onTypeMessage = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
         setMessage(event.nativeEvent.text);
@@ -32,39 +54,61 @@ const Message: React.FC <Props> = ({ navigation }) => {
 
     const onSendMessage = async () => {
         const newMessage = [...allMessages];
-        const userInfo = await getInformationsFromStorage('user');
 
-        if(userInfo)
+        newMessage.push({
+            from: userInfos.email,
+            message,
+            timestamp: new Date().getTime(),
+            state: MessageState.Wait
+        });
+        
+        setAllMessages(newMessage);
+
+        if(chatInfos.chatID === '')
         {
-            const { email } = JSON.parse(userInfo);
+            const wasCreated = await createChat();
 
-            newMessage.push({
-                from: email,
-                message,
-                timestamp: new Date().getTime(),
-                state: MessageState.Wait
+            if(!wasCreated)
+            {
+                return;
+            }
+        }
+
+        await sendMessage();
+        setMessage('');
+
+    }
+
+    const sendMessage = async () => {
+
+    }
+
+    const createChat = async (): Promise<boolean> => {
+        try
+        {
+            const token = await getInformationsFromStorage('authToken');
+
+            const { data } = await api.post<{ chatID: string }>('/messages/create-chat', {
+                userEmail: userInfos.email,
+                contactEmail: chatInfos.contactInfo.email,
+                headers: {
+                    'x-access-token': token?.replace(/"/g, '')
+                }
             });
-            
-            setAllMessages(newMessage);
 
-            initChat();
+            const chat = {
+                ...chatInfos,
+                chatID: data.chatID
+            };
+
+            setChatInfos(chat);
+            return true;
         }
-
-    }
-
-    const initChat = () => {
-        if(chatInfos.chatID !== "")
+        catch(err)
         {
-
+            Alert.alert('Algo inesperado aconteceu por aqui :(', 'Um erro inesperado ocorreu ao iniciar a conversa. Por favor, tente enviar a mensagem novamente');
+            return false;
         }
-        else
-        {
-            createChat();
-        }
-    }
-
-    const createChat = () => {
-
     }
 
     return (
