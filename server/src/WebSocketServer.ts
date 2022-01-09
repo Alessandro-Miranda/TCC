0b04bbbd4ffe7b803c894fc2d256f6414d265605
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-09 17:23:14 -0300

    Inciado a construção da obtenção de novas mensagens

--- server/src/WebSocketServer.ts
+++ server/src/WebSocketServer.ts
@@ -1,15 +1,20 @@
 import { Express } from 'express';
+import { doc, onSnapshot, Unsubscribe } from 'firebase/firestore';
 import { createServer, Server } from 'http';
 import { Server as SocketIoServer, Socket } from 'socket.io';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
+import { firestoreApp } from './config/firebaseConfig';
 import { CONNECTION, DISCONNECT, NEW_MESSAGE } from './constants/constants';
 import { IWebSocketServer } from './interfaces/IWebSocketServer';
+import { MessageBody } from './types/Message';
 import { SocketDisconnectReason } from './types/WsServer';
 
 export class WebSocketServer implements IWebSocketServer
 {
     public server: Server;
     public io: SocketIoServer;
+    public unsub: Unsubscribe = () => {};
+    public newMessages: MessageBody[] = [];
 
     constructor(server: Express)
     {
@@ -31,7 +36,22 @@ export class WebSocketServer implements IWebSocketServer
         console.log('New user connected')
     }
 
-    newMessage(chatID?: string): void
+    newMessage(chatID?: string, userEmail?: string): void
+    {
+        if(!chatID && userEmail)
+        {
+            this.getAllMessages(userEmail);
+        }
+        else if(chatID)
+        {
+            this.unsub = onSnapshot(doc(firestoreApp, 'chats', chatID), (doc) => {
+                this.newMessages.push(doc.data() as MessageBody);
+                this.io.emit(NEW_MESSAGE, this.newMessages);
+            });
+        }
+    }
+
+    getAllMessages(userEmail: string): void
     {
         
     }
@@ -39,5 +59,6 @@ export class WebSocketServer implements IWebSocketServer
     disconnect(reason: SocketDisconnectReason): void
     {
         console.log('User disconnected');
+        this.unsub();
     }
 }
