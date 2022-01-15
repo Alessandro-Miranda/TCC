2a8db80275d5d7aff7cce8846c5054d268f4bf38
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-15 18:13:50 -0300

    Modificado a interface da classe

--- server/src/interfaces/IWebSocketServer.ts
+++ server/src/interfaces/IWebSocketServer.ts
@@ -1,12 +1,9 @@
 import { Socket } from 'socket.io';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events';
-import { SocketDisconnectReason } from '../types/WsServer';
 
 export interface IWebSocketServer
 {
     listen(): void;
     connected(socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>): void;
-    newMessage(chatID?: string, userEmail?: string): void;
-    disconnect(reason: SocketDisconnectReason): void;
     getAllMessages(userEmail: string): void;
 }
