a3bcf82651c588bd9af205000c72e7917e2f4a83
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-15 18:13:24 -0300

    Criado a rota para obter todos os contatos do usuário

--- server/src/routes/messages.ts
+++ server/src/routes/messages.ts
@@ -3,7 +3,9 @@ import { MessageController } from '../controller/MessageController';
 
 const messagesRouter = Router();
 
-messagesRouter.get("/contacts/:email/:department", (req, res) => new MessageController().getContacts(req, res));
+messagesRouter.get("/contacts/:email", (req, res) => new MessageController().getContacts(req, res));
+
+messagesRouter.get("/contacts/:email/:department", (req, res) => new MessageController().getContactsBySameDepartment(req, res));
 
 messagesRouter.post("/create-chat", (req, res) => new MessageController().createChat(req, res));
 
