02a541a326e3763749af2a3f6d29045415aa3000
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-17 10:44:33 -0300

    Modificado a url para utilizar a constante da URL Base

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { BASE_URL } from '../constants';
 
 export const api = axios.create({
-    baseURL: 'http://192.168.1.2:4000/',
+    baseURL: BASE_URL,
     timeout: 10000
 });
