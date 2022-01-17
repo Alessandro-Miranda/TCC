b3b8e387c6466eb66ddb9b56ba8211b07eac10a4
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-17 15:17:02 -0300

    Implementado o método para obter os novos chats

--- server/src/WebSocketServer.ts
+++ server/src/WebSocketServer.ts
@@ -1,10 +1,11 @@
 import { Express } from 'express';
-import { doc, onSnapshot, Unsubscribe } from 'firebase/firestore';
+import { collection, doc, onSnapshot, Unsubscribe } from 'firebase/firestore';
 import { createServer, Server } from 'http';
 import { Server as SocketIoServer, Socket } from 'socket.io';
 import { firestoreApp } from './config/firebaseConfig';
-import { CONNECTION, DISCONNECT, NEW_MESSAGE } from './constants/constants';
+import { CONNECTION, DISCONNECT, NEW_CHAT, NEW_MESSAGE, USERS_COLLECTION_NAME } from './constants/constants';
 import { IWebSocketServer } from './interfaces/IWebSocketServer';
+import { Database } from './repositories/Database';
 import { MessageBody } from './types/Message';
 import { SocketDisconnectReason } from './types/WsServer';
 
@@ -16,7 +17,8 @@ export class WebSocketServer implements IWebSocketServer
 {
     public server: Server;
     public io: SocketIoServer;
-    static unsub: Unsubscribe = () => {};
+    static newMessageUnsub: Unsubscribe = () => {};
+    static newChatUnsub: Unsubscribe = () => {};
     public messages: MessageBody[] = [];
     
     constructor(server: Express)
@@ -34,8 +36,13 @@ export class WebSocketServer implements IWebSocketServer
     connected(socket: Socket): void
     {
         console.log('New user connected');
+
         socket.on(NEW_MESSAGE, (chatID) => {
             WebSocketServer.newMessage(chatID, socket);
+        });
+
+        socket.on(NEW_CHAT, (userEmail) => {
+            WebSocketServer.newChat(userEmail, socket);
         });
 
         socket.on(DISCONNECT, (reason) => {
@@ -45,7 +52,7 @@ export class WebSocketServer implements IWebSocketServer
 
     static newMessage(chatID: string, socket: Socket): void
     {
-        this.unsub = onSnapshot(doc(firestoreApp, 'chats', chatID, 'mensagens', 'mensagem'), (doc) => {
+        this.newMessageUnsub = onSnapshot(doc(firestoreApp, 'chats', chatID, 'mensagens', 'mensagem'), (doc) => {
             if(doc.exists())
             {
                 const messages = doc.data() as NewMessages;
@@ -54,14 +61,22 @@ export class WebSocketServer implements IWebSocketServer
         });
     }
 
-    getAllMessages(userEmail: string): void
+    static async newChat(userEmail: string, socket: Socket): Promise<void>
     {
-        
+        const db = new Database(USERS_COLLECTION_NAME);
+
+        this.newChatUnsub = onSnapshot(collection(firestoreApp, 'chats'), async snapshoot => {
+            
+            const chatsInfo = await db.getMessagePreview(snapshoot, userEmail);
+            
+            socket.emit(NEW_CHAT, chatsInfo);
+        })
     }
     
     static disconnect(reason: SocketDisconnectReason): void
     {
         console.log('User disconnected');
-        this.unsub();
+        this.newMessageUnsub();
+        this.newChatUnsub();
     }
 }
