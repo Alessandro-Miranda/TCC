2a619f3d7b5b8141c738b524c9158ecd2459c51d
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-17 14:22:15 -0300

    Inserido a constate de novo chat

--- server/src/constants/constants.ts
+++ server/src/constants/constants.ts
@@ -8,4 +8,5 @@ export const USERS_COLLECTION_NAME = 'usuarios';
 export const CONNECTION = 'connection';
 export const DISCONNECT = 'disconnect';
 export const NEW_MESSAGE = 'new message';
+export const NEW_CHAT = 'new chat';
 // End of Socket.io events
