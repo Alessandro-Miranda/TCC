328df87bd44b6448e3f17934293ee7077f0bf871
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-18 10:12:51 -0300

    Inserido as contantes utilizadas como nome das coleções'

---
+++ server/src/constants/index.ts
@@ -0,0 +1,18 @@
+// Auth constants to inform user search state
+export const USER_NOT_FOUND = 'User not found';
+export const INVALID_PASSWORD = 'invalid password';
+export const USERS_COLLECTION_NAME = 'usuarios';
+// End of Auth constants to inform user search state
+
+//Socket.io events
+export const CONNECTION = 'connection';
+export const DISCONNECT = 'disconnect';
+export const NEW_MESSAGE = 'new message';
+export const NEW_CHAT = 'new chat';
+// End of Socket.io events
+
+// Firebase collection names
+export const USERS = 'usuarios';
+export const CONTACTS = 'contatos';
+export const CHATS = 'chats';
+// End of Firebase collection names
