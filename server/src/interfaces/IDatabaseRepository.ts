7d82a64fe4779dbe69d55dabd60fadf7bd9a9318
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-01-18 10:27:35 -0300

    Atualizado o nome do método que obtém a referência de coleções

--- server/src/interfaces/IDatabaseRepository.ts
+++ server/src/interfaces/IDatabaseRepository.ts
@@ -5,7 +5,7 @@ import { User } from "../types/User";
 
 export interface IDatabaseRepositorie
 {
-    getRef(collectionName: string): CollectionReference<DocumentData>;
+    getCollectionRef(collectionName: string): CollectionReference<DocumentData>;
     findUserByEmail(email: string): Promise<User>;
     findAllUsersByDepartment(department: string): Promise<User[]>;
     findAllContacts(username: string): Promise<Contacts[]>;
