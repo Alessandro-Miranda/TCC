10b8cc680675c0476bdbfba3dc7cabe97440a871
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-02-06 17:22:37 -0300

    Corrigido o método para receber novos chats

--- server/src/WebSocketServer.ts
+++ server/src/WebSocketServer.ts
@@ -1,5 +1,5 @@
 import { Express } from 'express';
-import { onSnapshot, query, Unsubscribe, where } from 'firebase/firestore';
+import { onSnapshot, query, Unsubscribe } from 'firebase/firestore';
 import { createServer, Server } from 'http';
 import { Server as SocketIoServer, Socket } from 'socket.io';
 import {
@@ -73,19 +73,13 @@ export class WebSocketServer implements IWebSocketServer
     static async newChat(userEmail: string, socket: Socket): Promise<void>
     {
         const db = new Database();
-        const queryToExecute = query(db.getCollectionRef(CHATS), where('users', 'array-contains', userEmail));
+        const queryToExecute = query(db.getCollectionRef(CHATS));
         
         this.newChatUnsub = onSnapshot(queryToExecute, async snapshoot => {
             const chatsInfo = await db.getMessagePreview(snapshoot, userEmail);
             
             socket.emit(NEW_CHAT, chatsInfo);
         });
-        // this.newChatUnsub = onSnapshot(db.getCollectionRef('chats'), async snapshoot => {
-        //     console.log('chegou coisa nova aqui hein');
-        //     const chatsInfo = await db.getMessagePreview(snapshoot, userEmail);
-            
-        //     socket.emit(NEW_CHAT, chatsInfo);
-        // });
     }
     
     static disconnect(reason: SocketDisconnectReason): void
