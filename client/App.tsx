d623e4e604d732fe937b0d46403bf9f114e152e9
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2022-02-12 20:22:33 -0300

    Passado o Provider do contexto

--- client/App.tsx
+++ client/App.tsx
@@ -1,6 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { io, Socket } from 'socket.io-client';
+import { BASE_URL } from './src/constants';
+import { SocketContext } from './src/contexts/socketContext';
 import Routes from './src/routes';
 
-const App: FC = () => <Routes />;
+const App: FC = () => {
+
+    const [ socket, setSocket ] = useState<Socket>();
+
+    useEffect(() => {
+
+        const socket = io(BASE_URL);
+        
+        setSocket(socket);
+    }, []);
+
+    return (
+        <SocketContext.Provider value={{ socket } }>
+            <Routes />
+        </SocketContext.Provider>
+    )
+};
 
 export default App;
